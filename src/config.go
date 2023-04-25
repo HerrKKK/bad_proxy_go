34887d48c50c01d7cdca0c7e0f1e1375f20abd25
wang.weiran <wang.wei.ran@outlook.com>
2023-04-26 04:25:44 +1000

    trim error

--- src/config.go
+++ src/config.go
@@ -3,27 +3,17 @@ package main
 import (
 	"encoding/json"
 	"go_proxy/proxy"
-	"log"
 	"os"
-)
-
-type AppProtType string
-
-const (
-	HTTP AppProtType = "http"
-	BTP  AppProtType = "btp"
 )
 
 func ReadConfig(path string) (config proxy.Config, err error) {
 	file, err := os.ReadFile(path)
 	if err != nil {
-		log.Printf("failed to read file ")
-		return config, nil
+		return
 	}
 	err = json.Unmarshal(file, &config)
 	if err != nil {
-		log.Printf("failed to unmarshal")
-		return config, nil
+		return
 	}
 	return config, nil
 }
