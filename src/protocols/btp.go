58fea39c56d8ec145037435f144c9dbea7f360c4
wang.weiran <wang.wei.ran@outlook.com>
2023-04-26 03:11:49 +1000

    replace literal digits

--- src/protocols/btp.go
+++ src/protocols/btp.go
@@ -14,6 +14,22 @@ import (
 	"time"
 )
 
+const btpDigestLen = 32
+const btpConfusionLenDig = 1
+const btpTimestampLen = 4
+const btpDirectiveDig = 1
+const btpHostLenDig = 1
+const btpPortLen = 2
+const btpHeaderLen = btpDigestLen +
+	btpConfusionLenDig +
+	btpTimestampLen +
+	btpHostLenDig +
+	btpDirectiveDig +
+	btpPortLen
+const timeThreshold = 210
+const btpMaxConfusionLen = 64
+const btpTimeDiffRand = 30
+
 type BTPRequest struct {
 	Address      string
 	Payload      []byte
@@ -25,15 +41,15 @@ type BTPRequest struct {
 }
 
 func (request *BTPRequest) Validate(secret string) (err error) {
-	if request.confusionLen < 0 || request.confusionLen > 64 {
+	if request.confusionLen < 0 || request.confusionLen > btpMaxConfusionLen {
 		return errors.New("unexpected confusion length")
 	}
-	if request.timediff < -210 || 210 < request.timediff {
+	if request.timediff < -timeThreshold || timeThreshold < request.timediff {
 		return errors.New("timeout or replay attack")
 	}
 
 	h := hmac.New(sha256.New, []byte(secret))
-	h.Write(request.rawdata[32:])
+	h.Write(request.rawdata[btpDigestLen:])
 	if hex.EncodeToString(h.Sum(nil)) != request.digest {
 		return errors.New("digest mismatch, unauthorized connect")
 	}
@@ -44,7 +60,7 @@ func (request *BTPRequest) Validate(secret string) (err error) {
 		return err
 	}
 
-	if request.headerLen != 41 {
+	if request.headerLen != btpHeaderLen {
 		return errors.New("wrong btp head len")
 	}
 
@@ -53,20 +69,20 @@ func (request *BTPRequest) Validate(secret string) (err error) {
 
 func ParseBtpRequest(buffer []byte) (request *BTPRequest, err error) {
 	request = &BTPRequest{}
-	request.digest = hex.EncodeToString(buffer[:32])
-	request.confusionLen = int(buffer[32]) // uint8
+	request.digest = hex.EncodeToString(buffer[:btpDigestLen])
+	request.confusionLen = int(buffer[btpDigestLen]) // uint8
 
-	pos := 32 + 1 + int(request.confusionLen)
-	timestamp := binary.BigEndian.Uint32(buffer[pos : pos+4])
+	pos := btpDigestLen + btpConfusionLenDig + request.confusionLen
+	timestamp := binary.BigEndian.Uint32(buffer[pos : pos+btpTimestampLen])
 	request.timediff = int(time.Now().Unix()) - int(timestamp)
 
-	pos += 5 // 4 for timestamp 1 for directives
+	pos += btpTimestampLen + btpDirectiveDig
 	hostLen := int(buffer[pos])
-	pos++
+	pos += btpHostLenDig
 	host := string(buffer[pos : pos+hostLen])
-	pos += hostLen
-	port := strconv.Itoa(int(binary.BigEndian.Uint16(buffer[pos : pos+2])))
-	pos += 2
+	pos += hostLen // possible out of bound
+	port := strconv.Itoa(int(binary.BigEndian.Uint16(buffer[pos : pos+btpPortLen])))
+	pos += btpPortLen
 
 	request.headerLen = pos - request.confusionLen - hostLen
 	request.Address = host + ":" + port
@@ -77,25 +93,34 @@ func ParseBtpRequest(buffer []byte) (request *BTPRequest, err error) {
 
 func EncodeBtpRequest(address string, payload []byte, secret string) (res []byte, err error) {
 	bytesBuffer := bytes.NewBuffer([]byte{})
-	confusionLen, _ := rand.Int(rand.Reader, big.NewInt(64))
+	confusionLen, err := rand.Int(rand.Reader, big.NewInt(btpMaxConfusionLen))
+	if err != nil {
+		return
+	}
 	confusion := make([]byte, int(confusionLen.Int64()))
 	n, err := rand.Read(confusion)
 	if n != int(confusionLen.Int64()) || err != nil {
 		return
 	}
 
-	timeDiff, _ := rand.Int(rand.Reader, big.NewInt(60))
-	timestamp := time.Now().Unix() + timeDiff.Int64() - 30 // time +-30
+	timeDiff, err := rand.Int(rand.Reader, big.NewInt(2*btpTimeDiffRand))
+	if err != nil {
+		return
+	}
+	timestamp := time.Now().Unix() + timeDiff.Int64() - btpTimeDiffRand // time +-30
 
 	hnp := strings.Split(address, ":")
 	host := []byte(hnp[0])
-	port, _ := strconv.Atoi(hnp[1])
+	port, err := strconv.Atoi(hnp[1])
+	if err != nil {
+		return
+	}
 
 	_ = binary.Write(bytesBuffer, binary.BigEndian, uint8(confusionLen.Uint64()))
 	_ = binary.Write(bytesBuffer, binary.BigEndian, confusion)
 	_ = binary.Write(bytesBuffer, binary.BigEndian, uint32(timestamp))
-	_ = binary.Write(bytesBuffer, binary.BigEndian, uint8(1))         // directive
-	_ = binary.Write(bytesBuffer, binary.BigEndian, uint8(len(host))) // directive
+	_ = binary.Write(bytesBuffer, binary.BigEndian, uint8(1)) // directive
+	_ = binary.Write(bytesBuffer, binary.BigEndian, uint8(len(host)))
 	_ = binary.Write(bytesBuffer, binary.BigEndian, host)
 	_ = binary.Write(bytesBuffer, binary.BigEndian, uint16(port))
 	_ = binary.Write(bytesBuffer, binary.BigEndian, payload)
@@ -105,6 +130,6 @@ func EncodeBtpRequest(address string, payload []byte, secret string) (res []byte
 	h := hmac.New(sha256.New, []byte(secret))
 	h.Write(body)
 	digest := h.Sum(nil)
-
-	return append(digest, body...), nil
+	res = append(digest, body...)
+	return
 }
