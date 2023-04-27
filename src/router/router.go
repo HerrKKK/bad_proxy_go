e14b973fc474c5c3c64683bc4ef173cdc07a6791
wang.weiran <wang.wei.ran@outlook.com>
2023-04-27 17:02:04 +1000

    add matchers

--- src/router/router.go
+++ src/router/router.go
@@ -1,43 +1,82 @@
 package router
 
-import "strings"
+import (
+	"go_proxy/structure"
+	"regexp"
+)
 
 type Matcher interface {
-	Build() error
 	MatchAny(key string) bool
 }
 
-type Trie struct {
-	nodes   map[string]*Trie
-	failure *Trie
-	emit    bool
+type FullMatcher map[string]bool
+
+func (matcher *FullMatcher) MatchAny(key string) bool {
+	_, exist := (*matcher)[key]
+	return exist
 }
 
-type DomainMatcher struct { // AC automaton
-	root *Trie
+func NewFullMatcher(fullDomains []string) *Matcher {
+	var matcher Matcher
+	var fullMatcher FullMatcher
+	m := make(map[string]bool, len(fullDomains))
+	fullMatcher = m
+	for _, domain := range fullDomains {
+		fullMatcher[domain] = true
+	}
+	matcher = &fullMatcher
+	return &matcher
 }
 
-func NewDomainMatcher() (matcher *DomainMatcher) {
-	return &DomainMatcher{root: &Trie{nodes: make(map[string]*Trie)}}
+type RegexMatcher []*regexp.Regexp
+
+func NewRegexMatcher(RegexStrs []string) *Matcher {
+	var matcher Matcher
+	var regexMatcher RegexMatcher
+	regexMatcher = make([]*regexp.Regexp, len(RegexStrs))
+	for i, ex := range RegexStrs {
+		regexMatcher[i] = regexp.MustCompile(ex)
+	}
+	matcher = &regexMatcher
+	return &matcher
 }
 
-func (matcher *DomainMatcher) MatchAny(host string) bool {
-	tokens := strings.Split(host, ".")
-	curr := matcher.root
-
-	for i := len(tokens) - 1; i >= 0; i-- {
-		next, exist := curr.nodes[tokens[i]]
-		if exist == true {
-			curr = next
-		} else {
-			curr = curr.failure
-		}
-		if curr == matcher.root {
-			return false
-		}
-		if curr.emit == true {
+func (matcher *RegexMatcher) MatchAny(key string) bool {
+	for _, re := range *matcher {
+		if re.MatchString(key) {
 			return true
 		}
 	}
 	return false
+}
+
+func NewACAutomatonMatcher(domains []string) *Matcher {
+	var matcher Matcher
+	var acMatcher structure.ACAutomaton
+	acMatcher = *structure.NewACAutomaton(domains)
+	matcher = &acMatcher
+	return &matcher
+}
+
+type Router []Matcher
+
+func (router Router) MatchAny(key string) bool {
+	for _, m := range router {
+		if m.MatchAny(key) {
+			return true
+		}
+	}
+	return false
+}
+
+func NewRouter(
+	fullDomains []string,
+	RegexStrs []string,
+	domains []string,
+) *Router {
+	matchers := make([]Matcher, 0)
+	matchers = append(matchers, *NewFullMatcher(fullDomains))
+	matchers = append(matchers, *NewRegexMatcher(RegexStrs))
+	matchers = append(matchers, *NewACAutomatonMatcher(domains))
+	return (*Router)(&matchers)
 }
