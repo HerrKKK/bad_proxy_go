8b9a9d36aeba02d89a751bda344efa93ef0730b2
wwr <iswangwr@outlook.com>
2024-06-19 12:25:21 +0800

    [feature] Add attestation

--- build.sh
+++ build.sh
@@ -1,37 +1,42 @@
-cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1 && pwd)" || exit 1  # change to project directory
 declare -A build_targets
 build_targets=(["amd64"]="linux windows darwin" ["arm64"]="linux android darwin")
 rule_filename="rules.dat"
 dist_name=${1}
 
-function package_zip() {
-    zip "${dist_name}-${GOOS}-${GOARCH}.zip" "./${1}" ${rule_filename} config.json
-    sha256sum "${dist_name}-${GOOS}-${GOARCH}.zip" > "${dist_name}-${GOOS}-${GOARCH}.zip.sha256sum"
+function prepare() {
+    cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1 && pwd)" || exit 1  # change to project directory
+    if [ ! -d "./dist"  ]
+    then
+      mkdir ./dist
+    fi
+    wget https://github.com/HerrKKK/domain-list-community/releases/latest/download/${rule_filename}
 }
 
-function package_tar_gz() {
-    tar czvf "${dist_name}-${GOOS}-${GOARCH}.tar.gz" "./${1}" ${rule_filename} config.json
-    sha256sum "${dist_name}-${GOOS}-${GOARCH}.tar.gz" > "${dist_name}-${GOOS}-${GOARCH}.tar.gz.sha256sum"
+function main() {
+    prepare
+    for arch in ${!build_targets[*]}
+      do
+        go_os_array=${build_targets[$arch]}
+        for os in ${go_os_array[@]}
+        do
+          export GOARCH=${arch}
+          export GOOS=${os}
+          target="${dist_name}-${GOOS}-${GOARCH}"
+          if [ "${GOOS}" == "windows" ]
+          then
+            go build -o "./${target}.exe"
+            zip "${target}.zip" "./${target}.exe" ${rule_filename} config.json
+            go build -o "./${target}_cli.exe" -ldflags="-H windowsgui"
+            zip "${target}.zip" "./${target}_cli.exe" ${rule_filename} config.json
+          else
+            go build -o "./${target}"
+            tar czvf "${dist_name}-${GOOS}-${GOARCH}.tar.gz" "./${target}" ${rule_filename} config.json
+          fi
+        done
+      done
+
+    mv ./*.tar.gz ./dist
+    mv ./*.zip ./dist
 }
 
-wget https://github.com/HerrKKK/domain-list-community/releases/latest/download/${rule_filename}
-for arch in ${!build_targets[*]}
-  do
-    go_os_array=${build_targets[$arch]}
-    for os in ${go_os_array[@]}
-    do
-      export GOARCH=${arch}
-      export GOOS=${os}
-      filename="${dist_name}-${GOOS}-${GOARCH}"
-      if [ "${GOOS}" == "windows" ]
-      then
-        go build -o "./${filename}.exe"
-        package_zip "${filename}.exe"
-        go build -o "./${filename}_nogui.exe" -ldflags="-H windowsgui"
-        package_zip "${filename}_nogui.exe"
-      else
-        go build -o "./${filename}"
-        package_tar_gz "${filename}"
-      fi
-    done
-  done
+main
