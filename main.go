6107c1b9033182da87701741a6d4cd881844b453
wwr <me@wwr.icu>
2024-06-22 11:55:05 +0800

    [feature] Remove build functionality
    Signed-off-by: wwr <me@wwr.icu>

--- main.go
+++ main.go
@@ -5,12 +5,11 @@ import (
 	"flag"
 	"fmt"
 	"go_proxy/proxy"
-	"go_proxy/router"
 	"os"
 )
 
 const (
-	versionMsg = "v1.0.3"
+	versionMsg = "v1.0.4"
 	helpMsg    = `
 	Bad proxy go is a primitive tool for breaching censorship
 	Run application:
@@ -20,12 +19,6 @@ const (
 		--version           show version message
 		--config            specify config file path, default conf/config.json
 		--router-path       specify binary routing file path, default conf/rules.dat
-
-	Build routing file from text to binary:
-		bad_proxy build <options>
-	Options:
-		--rule-path         specify text rule files directory path, default ./rules
-		--router-path       specify output binary routing file path, default conf/rules.dat
 	`
 )
 
@@ -44,19 +37,9 @@ func ReadConfig(path string) (config proxy.Config, err error) {
 func main() {
 	helpFlag := flag.Bool("help", false, "show help info")
 	versionFlag := flag.Bool("version", false, "show version info")
+	configPath := flag.String("config", "config.json", "config file path")
+	routerPath := flag.String("router-path", "rules.dat", "router data path")
 	flag.Parse()
-
-	if len(os.Args) >= 2 && os.Args[1] == "build" {
-		if err := build(); err != nil {
-			fmt.Println(err)
-			fmt.Print(helpMsg)
-		}
-		return
-	}
-
-	if err := run(); err != nil {
-		fmt.Print("failed to run app")
-	}
 
 	if *versionFlag == true {
 		fmt.Println("Bad Proxy Golang", versionMsg)
@@ -66,34 +49,15 @@ func main() {
 		fmt.Print(helpMsg)
 		return
 	}
-}
-
-func run() (err error) {
-	configPath := flag.String("config", "config.json", "config file path")
-	routerPath := flag.String("router-path", "rules.dat", "router data path")
-	flag.Parse()
 
 	config, err := ReadConfig(*configPath)
 	if err != nil {
 		fmt.Print("failed to read config\n")
 		return
 	}
+
 	config.RouterPath = *routerPath
 	proxy.NewProxy(config).Start()
 	<-make(chan os.Signal)
-	return
-}
-
-func build() (err error) {
-	buildCmd := flag.NewFlagSet("build", flag.ExitOnError)
-	rulePath := flag.String("rule-path", "./rules", "router data path")
-	routerPath := flag.String("router-path", "rules.dat", "router data path")
-	if err = buildCmd.Parse(os.Args[2:]); err != nil {
-		fmt.Println(err)
-		return
-	}
-	if err = router.WriteAllToGob(*rulePath, *routerPath); err == nil {
-		fmt.Printf("success build from %s to %s\n", *rulePath, *routerPath)
-	}
 	return
 }
